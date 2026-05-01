ef83ca293a3ebdca573cb5a44dc1d59c8d38d521
O-Khalil <217347337+O-Khalill@users.noreply.github.com>
2026-05-01 23:25:00 +0300

    changed the sleep timers to make it more user friendly, postponed path finding algorithms for now

--- src/Algorithms.svelte
+++ src/Algorithms.svelte
@@ -1,57 +1,53 @@
 <script lang="ts">
-     import { Button } from "$lib/components/ui/button";
-     import Sorting from "./Sorting.svelte";
-     import Searching from "./Searching.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import Sorting from "./Sorting.svelte";
+  import Searching from "./Searching.svelte";
 
-     let selected = $state("");
+  let selected = $state("");
 
-     let array = $state<number[]>([]);
-     let target = $state();
+  let array = $state<number[]>([]);
+  let target = $state();
 
-     function genArray() {
-          array = Array.from({ length: 15 }, () =>
-               Math.ceil(Math.random() * 50),
-          );
-     }
-     function genArrayWithTarget() {
-          array = Array.from({ length: 15 }, () =>
-               Math.ceil(Math.random() * 50),
-          );
+  function genArray() {
+    array = Array.from({ length: 15 }, () => Math.ceil(Math.random() * 50));
+  }
+  function genArrayWithTarget() {
+    array = Array.from({ length: 15 }, () => Math.ceil(Math.random() * 50));
 
-          target = array[Math.floor(Math.random() * array.length)];
-     }
+    target = array[Math.floor(Math.random() * array.length)];
+  }
 </script>
 
 <div class="flex flex-col items-center">
-     <h1 class="text-4xl font-extrabold text-[#ff3e00] tracking-wide">
-          Choose Your Algorithm Category
-     </h1>
-     <div class="flex justify-evenly gap-3">
-          <Button
-               onclick={() => {
-                    selected = "sorting";
-                    genArray();
-               }}
-               class="px-6 py-3 rounded-xl font-bold text-white border-2 border-[#ff3e00] hover:bg-[#ff3e00] transition-colors duration-200 my-3 text-lg tracking-wide {selected ===
-               'sorting'
-                    ? 'bg-[#ff3e00]'
-                    : 'bg-transparent'}"
-          >
-               Sorting
-          </Button>
-          <Button
-               onclick={() => {
-                    selected = "searching";
-                    genArrayWithTarget();
-               }}
-               class="px-6 py-3 rounded-xl font-bold text-white border-2 border-[#ff3e00] hover:bg-[#ff3e00] transition-colors duration-200 my-3 text-lg tracking-wide {selected ===
-               'searching'
-                    ? 'bg-[#ff3e00]'
-                    : 'bg-transparent'}"
-          >
-               Searching
-          </Button>
-          <Button
+  <h1 class="text-4xl font-extrabold text-[#ff3e00] tracking-wide">
+    Choose Your Algorithm Category
+  </h1>
+  <div class="flex justify-evenly gap-3">
+    <Button
+      onclick={() => {
+        selected = "sorting";
+        genArray();
+      }}
+      class="px-6 py-3 rounded-xl font-bold text-white border-2 border-[#ff3e00] hover:bg-[#ff3e00] transition-colors duration-200 my-3 text-lg tracking-wide {selected ===
+      'sorting'
+        ? 'bg-[#ff3e00]'
+        : 'bg-transparent'}"
+    >
+      Sorting
+    </Button>
+    <Button
+      onclick={() => {
+        selected = "searching";
+        genArrayWithTarget();
+      }}
+      class="px-6 py-3 rounded-xl font-bold text-white border-2 border-[#ff3e00] hover:bg-[#ff3e00] transition-colors duration-200 my-3 text-lg tracking-wide {selected ===
+      'searching'
+        ? 'bg-[#ff3e00]'
+        : 'bg-transparent'}"
+    >
+      Searching
+    </Button>
+    <!-- <Button
                onclick={() => {
                     selected = "path";
                }}
@@ -61,14 +57,14 @@
                     : 'bg-transparent'}"
           >
                Path finding
-          </Button>
-     </div>
+          </Button> -->
+  </div>
 
-     {#if selected === "sorting"}
-          <Sorting {array} />
-     {:else if selected === "searching"}
-          <Searching {array} {target} />
-     {:else if selected === "path"}
-          <!-- <Pathfinding /> -->
-     {/if}
+  {#if selected === "sorting"}
+    <Sorting {array} />
+  {:else if selected === "searching"}
+    <Searching {array} {target} />
+  {:else if selected === "path"}
+    <!-- <Pathfinding /> -->
+  {/if}
 </div>
--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,23 @@
 <script>
-     import { Router, Route } from "svelte-routing";
-     import Head from "./lib/Head.svelte";
-     import Hero from "./lib/Hero.svelte";
-     import Algorithms from "./Algorithms.svelte";
-     import Particle from "./Particle.svelte";
+  import { Router, Route } from "svelte-routing";
+  import Head from "./lib/Head.svelte";
+  import Hero from "./lib/Hero.svelte";
+  import Algorithms from "./Algorithms.svelte";
+  import Particle from "./Particle.svelte";
 </script>
 
 <Router>
-     <div class="bg-[#121212] h-screen w-full flex flex-col bg-grid scanlines">
-          <!-- <div
-               class="fixed top-25 left-20 w-42 h-42 bg-[#ff3e00] rounded-full opacity-40 blur-[110px] pointer-events-none animate-blob"
-          ></div>
+  <div class="bg-[#121212] h-screen w-full flex flex-col bg-grid scanlines">
+    <Head />
+    <Route path="/">
+      <div class="flex items-center flex-1 justify-center">
+        <Hero />
+      </div>
+    </Route>
+    <Route path="/algorithms">
+      <Algorithms />
+    </Route>
 
-          <div
-               class="fixed bottom-25 right-20 w-42 h-72 bg-[#ff3e00] rounded opacity-40 blur-[110px] pointer-events-none animate-blob"
-          ></div>
- -->
-          <Head />
-          <Route path="/">
-               <div class="flex items-center flex-1 justify-center">
-                    <Hero />
-               </div>
-          </Route>
-          <Route path="/algorithms">
-               <Algorithms />
-          </Route>
-
-          <Particle />
-     </div>
+    <Particle />
+  </div>
 </Router>
